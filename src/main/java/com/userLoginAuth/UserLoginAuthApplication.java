ff01538e9a23f53f4d3322126ced149fc260e507
daveeclark <dclark2016@hotmail.com>
2019-12-10 13:24:10 -0500

    commit

---
+++ src/main/java/com/userLoginAuth/UserLoginAuthApplication.java
@@ -0,0 +1,13 @@
+package com.userLoginAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userLoginAuth/UserLoginAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userLoginAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
